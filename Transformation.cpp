19189b7302191bb85172d8275824f818a00c6059
Sergey Doroshin <sergeybezzabotnovv@gmail.com>
2022-10-29 10:11:11 +0300

    Openmp

--- Transformation.cpp
+++ Transformation.cpp
@@ -4,41 +4,12 @@
 
 #include <chrono>
 
-namespace
-{
-using std::chrono::steady_clock;
-using std::chrono::duration_cast;
-using std::chrono::duration;
-using std::chrono::milliseconds;
-
-void transformMatrix(Matrix& inMatrix, Matrix& outMatrix)
-{
-    uint8_t result;
-    for (size_t columnIndex = 0; columnIndex < M; ++columnIndex)
-    {
-        for (size_t i = 0; i < M; ++i)
-        {
-            result = 0;
-            for (size_t j = 0; j < M; ++j)
-            {
-                result = plus(result, mult(coeffs[i][j], inMatrix[j][columnIndex]));
-            }
-            outMatrix[i][columnIndex] = result;
-        }
-    }
-}
-
-void transformDataBatch(VecMatrixIt inBegin, VecMatrixIt inEnd, VecMatrixIt outBegin, VecMatrixIt outEnd)
-{
-    for (auto in = inBegin, out = outBegin; in != inEnd; ++in, ++out)
-        transformMatrix(*in, *out);
-}
-}
-
 void transformData(size_t N, std::string inFileName, std::string outFileName)
 {
     VecMatrix inMatrixes(batchSize, Matrix());
     VecMatrix outMatrixes(batchSize, Matrix());
+
+    uint8_t result;
     size_t done = 0;
     size_t duration = 0;
     FileWriter writer(outFileName);
@@ -50,18 +21,30 @@ void transformData(size_t N, std::string inFileName, std::string outFileName)
 
     while (done < N)
     {
-        size_t count = std::min(batchSize, N - done);
+        reader.deserializeDataBatch(inMatrixes.begin(), inMatrixes.end());
+        auto startTimeStamp = std::chrono::steady_clock::now();
 
-        reader.deserializeDataBatch(inMatrixes.begin(), inMatrixes.begin() + count);
-        auto startTimeStamp = steady_clock::now();
-        transformDataBatch(inMatrixes.begin(), inMatrixes.begin() + count, outMatrixes.begin(), outMatrixes.begin() + count);
-        auto stopTimeStamp = steady_clock::now();
-        writer.serializeDataBatch(outMatrixes.begin(), outMatrixes.begin() + count);
+#pragma omp parallel
+        {
+#pragma omp for
+            for (int i = 0; i < batchSize * M; ++i)
+            {
+                for (size_t k = 0; k < M; ++k)
+                {
+                    result = 0;
+                    for (size_t j = 0; j < M; ++j)
+                        result = plus(result, mult(coeffs[k][j], inMatrixes[i / M][j][i % M]));
 
-        done += count;
-        duration += static_cast<size_t>(duration_cast<milliseconds>(stopTimeStamp - startTimeStamp).count());
+                    outMatrixes[i / M][k][i % M] = result;
+                }
+            }
+        }
+
+        auto stopTimeStamp = std::chrono::steady_clock::now();
+        writer.serializeDataBatch(outMatrixes.begin(), outMatrixes.end());
+        done += batchSize;
+        duration += static_cast<size_t>(std::chrono::duration_cast<std::chrono::milliseconds>(stopTimeStamp - startTimeStamp).count());
     }
-
     writer.serializeTimeStamp(duration);
 
     writer.finishWriting();
